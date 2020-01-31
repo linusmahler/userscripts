3634967e933eae7e53be16619ca6725adc36006c
Rickard Lindahl <rickard.lindahl.ext@siemens.com>
2020-01-31 08:32:05 +0100

    Rewrite to TypeScript

--- spark-jira-dashboard/src/components/seo.tsx
+++ spark-jira-dashboard/src/components/seo.tsx
@@ -6,11 +6,15 @@
  */
 
 import React from "react"
-import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, title }) {
+const SEO: React.FC<{
+  description?: string,
+  lang?: string,
+  meta?: JSX.IntrinsicElements['meta'][],
+  title: string,
+}> = ({ description = ``, lang = `en`, meta = [], title }) => {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -67,22 +71,10 @@ function SEO({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
-      ].concat(meta)}
+        ...meta,
+      ]}
     />
   )
-}
-
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
 }
 
 export default SEO
