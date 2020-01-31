ae15e64fdfd3046cd85fbb5a7e8f7d1918de7679
Rickard Lindahl <rickard.lindahl.ext@siemens.com>
2020-01-31 10:18:55 +0100

    Remove footer

--- spark-jira-dashboard/src/components/layout.tsx
+++ spark-jira-dashboard/src/components/layout.tsx
@@ -32,11 +32,6 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
       </div>
     </>
   )
